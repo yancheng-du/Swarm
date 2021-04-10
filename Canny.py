4fdec2a6c2f728f56c326b3b36cbfe63c4fe125f
chucky1729 <charliegough@utexas.edu>
2021-04-10 16:52:21 -0700

    adding read timing

--- Canny.py
+++ Canny.py
@@ -6,16 +6,29 @@ import cv2
 # np is an alias pointing to numpy library
 import numpy as np
 
+# ~ms resolution timer
+from timeit import default_timer as timer
 
 # capture frames from a camera
-cap = cv2.VideoCapture(1)
+for camera in range(2):
+	cap = cv2.VideoCapture(camera)
+	if cap.isOpened():
+		break
+if cap is None or not cap.isOpened():
+	sys.exit("no camera")
 
+# total time reading and number of times read
+read_total = 0
+read_count = 0
 
 # loop runs if capturing has been initialized
 while (1):
-
+	read_start = timer()
 	# reads frames from a camera
 	ret, frame = cap.read()
+	read_total += timer() - read_start
+	read_count += 1
+
 	blur = cv2.GaussianBlur(frame, (7,7),0)
 	#fgmask = fgbg.apply(blur)
 	# converting BGR to HSV
@@ -38,8 +51,8 @@ while (1):
 	# finds edges in the input image image and
 	# marks them in the output map edges
 	edges = cv2.Canny(blur, 200, 200)
-	print(edges.shape)
-	print(np.count_nonzero(edges))
+	#print(edges.shape)
+	#print(np.count_nonzero(edges))
 	# Display edges in a frame
 	cv2.imshow('Edges', edges)
 
@@ -47,6 +60,8 @@ while (1):
 	k = cv2.waitKey(5)&0xFF
 	if k==27:
 		break
+
+print("read average: ", 1000*read_total//read_count, "ms", sep="")
 
 # Close the window
 cap.release()
