44d50ad7a6300bf426ed4c8a7523bae3536b118e
Yancheng Du <duyc88@gmail.com>
2021-04-10 20:24:46 -0500

    Simple bee avoidance, bees will not land close to other bees

--- protoswarm/bees.py
+++ protoswarm/bees.py
@@ -4,7 +4,6 @@ import random
 
 from constants import *
 from entity import *
-
 ######## constants ########
 
 BEE_SIZE= 2.0
@@ -15,6 +14,7 @@ BEE_SPEED= 150.0
 BEE_INITIAL_COUNT= 1200
 BEE_COUNT_RATE= 300
 
+BEE_DISTANCE = 3
 ######## code ########
 
 class c_bee(c_entity):
@@ -27,10 +27,10 @@ class c_bee(c_entity):
 		c_entity.__init__(self, BEE_SIZE, BEE_COLOR, linear_position, c_vector2d(), random.uniform(0.0, 2*math.pi), 0)
 		self.timer= 0.0
 
-	def update(self, frame):
+	def update(self, frame,bee_map):
 		x= int(self.linear_position.x)
 		y= int(self.linear_position.y)
-		if x<0 or y<0 or x>=SIMULATION_BOUNDS[0] or y>=SIMULATION_BOUNDS[1] or frame[x, y]==0:
+		if x<0 or y<0 or x>=SIMULATION_BOUNDS[0] or y>=SIMULATION_BOUNDS[1] or frame[x, y]==0 or bee_map[x,y]==1:
 			if self.timer<=0.0:
 				self.timer= random.uniform(*BEE_CHANGE_TIME)
 				self.angular_velocity= random.uniform(*BEE_TURN_RATE)
@@ -39,12 +39,14 @@ class c_bee(c_entity):
 			self.linear_velocity= c_vector2d(facing=self.angular_position, magnitude=BEE_SPEED)
 		else:
 			self.linear_velocity= c_vector2d()
+			bee_map[max(x-BEE_DISTANCE,0):min(x+BEE_DISTANCE,SIMULATION_BOUNDS[0]),max(y-BEE_DISTANCE,0):min(y+BEE_DISTANCE,SIMULATION_BOUNDS[1])]= 1
 		c_entity.update(self)
 
 class c_bees(object):
 	def __init__(self, see_nectar= False):
 		self.desired_bee_count= BEE_INITIAL_COUNT
 		self.bees= []
+		self.bee_map = np.zeros((SIMULATION_BOUNDS[0],SIMULATION_BOUNDS[1]) )
 		self.see_nectar= see_nectar
 
 	def update(self, frame):
@@ -54,7 +56,8 @@ class c_bees(object):
 			elif len(self.bees)>self.desired_bee_count:
 				self.bees.pop()
 		for bee in self.bees:
-			bee.update(frame)
+			bee.update(frame,self.bee_map)
+		self.bee_map = np.zeros((SIMULATION_BOUNDS[0],SIMULATION_BOUNDS[1]) )
 
 	def draw(self, display):
 		display.fill(BLACK_COLOR)
