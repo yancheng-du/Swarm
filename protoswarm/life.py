e849c0a27ca3d02b63c5e9a3b29115eace5a5b8b
chucky1729 <charliegough@utexas.edu>
2021-04-11 03:30:30 -0700

    added conway's game of life

--- protoswarm/life.py
+++ protoswarm/life.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python
 
+import numpy as np
+import pygame
+
+from constants import *
+
 ######## code ########
 
 class c_life(object):
 	def __init__(self):
-		pass
+		self.board= np.zeros((SIMULATION_BOUNDS[1], SIMULATION_BOUNDS[0]))>0
 
 	def update(self, frame):
-		pass
+		self.board|= frame>0
+		count= sum(np.roll(np.roll(self.board, i, 0), j, 1) \
+			for i in (-1, 0, 1) for j in (-1, 0, 1) if (i != 0 or j != 0))
+		self.board= (count==3)|(self.board&(count==2))
 
 	def draw(self, display):
-		center= (SIMULATION_BOUNDS[0]//2, SIMULATION_BOUNDS[1]//2)
-		radius= math.min(SIMULATION_BOUNDS[0], SIMULATION_BOUNDS[1])//2
-		pygame.draw.circle(display, WHITE_COLOR, center, radius)
+		surface= pygame.pixelcopy.make_surface(255*self.board.T)
+		display.blit(surface, (0, 0))
