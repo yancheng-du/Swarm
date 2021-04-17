09b302bebbd6355193b4553f5efa3780dcc24797
Raghav <raghavc@utexas.edu>
2021-04-17 18:33:04 -0500

    Added pure canny as another mode

---
+++ protoswarm/canny.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+#Don't know if this is actually necessary, but thought it would be consistent with the other modes
+#Just spits back out camera-processing as a mode
+
+######## imports ########
+import numpy as np
+import pygame
+
+######## code ########
+
+
+class c_canny():
+    def __init__(self):
+        self.frame = 0
+
+    def update(self, frame):
+        self.frame = np.rot90(frame)
+
+    def draw(self, display):
+        surface = pygame.surfarray.make_surface(self.frame)
+        display.blit(surface, (0,0))
