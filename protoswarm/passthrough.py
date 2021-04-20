0f26b9ff290dc1981fe69bae138a7bcbd61d1d2f
Raghav <raghavc@utexas.edu>
2021-04-20 17:37:15 -0500

    Transposed instead of flip+rotate,fixed color/name

--- protoswarm/passthrough.py
+++ protoswarm/passthrough.py
@@ -6,17 +6,18 @@
 ######## imports ########
 import numpy as np
 import pygame
+import cv2
 
 ######## code ########
 
 
-class c_canny():
+class c_passthrough():
     def __init__(self):
         self.frame = 0
 
     def update(self, frame):
-        self.frame = np.rot90(frame)
-        self.frame = np.flip(self.frame, 0)
+        self.frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        self.frame = cv2.transpose(self.frame)
 
     def draw(self, display):
         surface = pygame.surfarray.make_surface(self.frame)
