2745eafd5725c08a28d2863aa111e0310352965a
Raghav <raghavc@utexas.edu>
2021-04-20 17:45:16 -0500

    Fixed formatting

--- protoswarm/passthrough.py
+++ protoswarm/passthrough.py
@@ -13,12 +13,12 @@ import cv2
 
 class c_passthrough():
     def __init__(self):
-        self.frame = 0
+        self.frame= 0
 
     def update(self, frame):
-        self.frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        self.frame = cv2.transpose(self.frame)
+        self.frame= cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        self.frame= cv2.transpose(self.frame)
 
     def draw(self, display):
-        surface = pygame.surfarray.make_surface(self.frame)
+        surface= pygame.surfarray.make_surface(self.frame)
         display.blit(surface, (0,0))
