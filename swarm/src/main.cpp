00be1be0f5a86a35c50c7ff28596316f51585d3e
chucky1729 <charliegough@utexas.edu>
2021-10-04 00:54:04 -0700

    added mac libtensorflow libs limited frame rate

--- swarm/src/main.cpp
+++ swarm/src/main.cpp
@@ -4,29 +4,95 @@
 #include "constants.h"
 #include "graphics.h"
 #include "swarm.h"
-#include <tensorflow/c/c_api.h>
+
+class timer_t
+{
+public:
+	timer_t()
+	{
+		frequency= static_cast<double>(SDL_GetPerformanceFrequency());
+		reset();
+	}
+
+	void reset()
+	{
+		counter=SDL_GetPerformanceCounter();
+	}
+
+	bool passed(double time)
+	{
+		return (SDL_GetPerformanceCounter()-counter)/frequency>time;
+	}
+
+private:
+	double frequency;
+	uint64_t counter;
+};
 
 int main(int argc, char *argv[])
 {
-	if (SDL_Init(SDL_INIT_VIDEO)==0)
+	int result= EXIT_SUCCESS;
+
+	if (SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO|SDL_INIT_EVENTS)==0)
 	{
+		bool running= true;
+		timer_t timer;
+
+		#ifdef DEBUG
+		SDL_LogSetAllPriority(SDL_LOG_PRIORITY_WARN);
+		#endif
+
 		camera_initialize();
 		graphics_initialize();
 
 		swarm_t swarm;
 
-		while (true)
+		while (running)
 		{
-			SDL_Event event;
-
-			SDL_PollEvent(&event);
-
-			if (event.type==SDL_QUIT)
+			if (SDL_WaitEventTimeout(NULL, 1))
 			{
-				break;
+				SDL_Event event;
+
+				while (SDL_PollEvent(&event))
+				{
+					switch (event.type)
+					{
+						case SDL_QUIT:
+						{
+							running= false;
+							break;
+						}
+
+						case SDL_KEYDOWN:
+						{
+							SDL_Keycode code= event.key.keysym.sym;
+
+							switch (code)
+							{
+								case SDLK_ESCAPE:
+								{
+									running= false;
+									break;
+								}
+
+								// $TODO add other key handling events here
+							}
+							break;
+						}
+
+						default:
+						{
+							SDL_LogInfo(SDL_LOG_CATEGORY_INPUT, "Received event type 0x%x", event.type);
+						}
+					}
+				}
 			}
-			else
+
+			if (timer.passed(1.0/k_fps-0.001))
 			{
+				while (!timer.passed(1.0/k_fps)) ;
+				timer.reset();
+
 				video_frame_t video_frame;
 				depth_frame_t depth_frame;
 				edge_frame_t edge_frame;
@@ -39,11 +105,14 @@ int main(int argc, char *argv[])
 
 		graphics_dispose();
 		camera_dispose();
+
+		SDL_Quit();
 	}
 	else
 	{
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
+		result= EXIT_FAILURE;
 	}
 
-	return 0;
+	return result;
 }
