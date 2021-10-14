ec04c3e42896afec5a6dae34e16510f93d1f571e
Yancheng Du <duyc88@gmail.com>
2021-10-14 16:09:35 -0500

    local commit attraction

--- swarm/src/camera.h
+++ swarm/src/camera.h
@@ -8,4 +8,7 @@ void camera_dispose();
 
 int camera_read_frame(cv::Mat3b *video_frame, cv::Mat1w *depth_frame, cv::Mat1b *edge_frame);
 
+void get_vector_frame(cv::Mat1b* edge_frame, cv::Mat* x_vector_frame, cv::Mat* y_vector_frame, size_t field_size);
+void init_field(size_t field_size);
+
 #endif /* camera_h */
