fd162c74b0fc77c806782762c98fea37a08b7baa
chucky1729 <charliegough@utexas.edu>
2021-10-15 14:07:33 -0700

    added audio skeleton

---
+++ swarm/src/audio.cpp
@@ -0,0 +1,18 @@
+#include "audio.hpp"
+
+bool audio_initialize()
+{
+	// $TODO intialize SDL audio subsystem, load sounds, and setup globals
+
+	return false;
+}
+
+void audio_dispose()
+{
+	// $TODO reset globals, free sounds, shutdown SDL audio system
+}
+
+void audio_render(const swarm_t *swarm)
+{
+	// $TODO analyze bees, start, update, and stop sounds
+}
