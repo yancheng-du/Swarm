3f6d39370f621f3b1dc6cf1b6876d274abdb8027
chucky1729 <charliegough@utexas.edu>
2021-10-21 15:35:04 -0700

    refactored main into main loop and director class

---
+++ swarm/src/director.hpp
@@ -0,0 +1,34 @@
+#ifndef director_hpp
+#define director_hpp
+
+#include <opencv2/core.hpp>
+
+#include "gesture.hpp"
+#include "swarm.hpp"
+
+class director_t
+{
+public:
+	director_t();
+	~director_t();
+
+	bool is_running();
+	void do_frame();
+	void process_events();
+
+private:
+	bool running= true;
+	bool idle= false;
+	bool debug= false;
+	bool fps= false;
+
+	cv::Mat3b video_frame;
+	cv::Mat3b last_video_frame;
+	cv::Mat1w depth_frame;
+	cv::Mat1b edge_frame;
+	commands_t commands;
+
+	swarm_t swarm;
+};
+
+#endif /* director_hpp */
