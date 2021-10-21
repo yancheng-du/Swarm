96523dcb93bd6df1cafe0b9e9550f722dc6e3f47
chucky1729 <charliegough@utexas.edu>
2021-10-21 09:41:10 -0700

    tidied up a little

--- swarm/src/audio.cpp
+++ swarm/src/audio.cpp
@@ -3,50 +3,41 @@
 #include "audio.hpp"
 #include "camera.hpp"
 
-// Continuous sounds
-Mix_Music *buzz= NULL;
-// Sound effects
-
-bool load_media()
-{
-    buzz= Mix_LoadMUS("res/wavfiles/beeswarm_mid.mp3");
-    if(buzz == NULL)
-    {
-        printf("%s\n", Mix_GetError());
-        return false;
-    }
-    return true;
-}
+Mix_Music *g_buzz_sound= NULL;
 
 bool audio_initialize()
 {
 	bool success= false;
+
 	if(Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048)  >= 0)
 	{
-        success= true;
+		g_buzz_sound= Mix_LoadMUS("res/wavfiles/beeswarm_mid.mp3");
+
+		if (g_buzz_sound)
+		{
+			success= true;
+		}
+		else
+		{
+			SDL_LogError(SDL_LOG_CATEGORY_AUDIO, "Couldn't load audio: %s", Mix_GetError());
+		}
 	}
-    else
-    {
-        SDL_LogError(SDL_LOG_CATEGORY_AUDIO, "Couldn't initialize audio mixer: %s", Mix_GetError());
-        success= false;
-    }
-    if(load_media())
-    {
-        success= true;
-    }
-    else
-    {
-        success= false;
-    }
+	else
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_AUDIO, "Couldn't initialize audio mixer: %s", Mix_GetError());
+	}
 
 	return success;
 }
 
 void audio_dispose()
 {
-	// $TODO reset globals and free sounds
-    Mix_FreeMusic(buzz);
-    buzz=NULL;
+	if (g_buzz_sound)
+	{
+		Mix_FreeMusic(g_buzz_sound);
+		g_buzz_sound=NULL;
+	}
+
 	while (Mix_Init(0))
 	{
 		Mix_Quit();
@@ -55,12 +46,12 @@ void audio_dispose()
 
 void audio_render(const swarm_t *swarm)
 {
-	// $TODO analyze bees, start, update, and stop sounds
-    // volume control
-    int volume= (get_distance()/get_avg_distance())*64;
-    Mix_VolumeMusic(volume);
-    if(Mix_PlayingMusic() == 0)
-    {
-        Mix_PlayMusic(buzz, -1);
-    }
+	int volume= (get_distance()/get_avg_distance())*64;
+
+	Mix_VolumeMusic(volume);
+
+	if (Mix_PlayingMusic()==0)
+	{
+		Mix_PlayMusic(g_buzz_sound, -1);
+	}
 }
